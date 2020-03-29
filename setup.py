2e7b17bbadaeace5bcfb16797ed8c5f96d15e952
Stefan <bruthaler@protonmail.com>
2020-03-29 15:33:51 +0200

    Updates build for 0.0.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='daily-reflection',
-     version='0.0.2',
+     version='0.0.4',
      scripts=['daily-reflection'] ,
      author="Stefan Thaler",
      author_email="bruthaler@gmail.com",

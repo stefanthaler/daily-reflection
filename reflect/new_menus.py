f4cef785d08abccf48081f4b9b757b3a1f280e04
Stefan <bruthaler@protonmail.com>
2020-04-24 09:39:12 +0200

    Adds catch key events to new menus

--- reflect/new_menus.py
+++ reflect/new_menus.py
@@ -1,7 +1,8 @@
 from prompt_toolkit import prompt
 from prompt_toolkit.filters import Condition
 from prompt_toolkit.key_binding import KeyBindings
-
+from prompt_toolkit import PromptSession
+from prompt_toolkit.patch_stdout import patch_stdout
 
 if __name__ == "__main__":
     bindings = KeyBindings()
@@ -11,9 +12,22 @@ if __name__ == "__main__":
     #     " Only activate key binding on the second half of each minute. "
     #     return datetime.datetime.now().second > 30
 
-    @bindings.add('c')
+    @bindings.add('<any>')
     def _(event):
         print(event)
         pass
+    # @bindings.add('c-m')
+    # def _(event):
+    #     print("Enter",event)
+    #     pass
+# wait for keypress
+    session = PromptSession()
+    while True:
+        session.prompt(key_bindings=bindings)
 
-    prompt('> ', key_bindings=bindings)
+
+
+
+
+
+# colorise output

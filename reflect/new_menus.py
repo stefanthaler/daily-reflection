3d25df036b31fe3eccf41b9d80c9b3b8c0bcf466
Stefan <bruthaler@protonmail.com>
2020-04-26 10:00:55 +0200

    Adds colorized output

--- reflect/new_menus.py
+++ reflect/new_menus.py
@@ -4,6 +4,31 @@ from prompt_toolkit.key_binding import KeyBindings
 from prompt_toolkit import PromptSession
 from prompt_toolkit.patch_stdout import patch_stdout
 
+
+# colorise output
+from prompt_toolkit.shortcuts import prompt
+from prompt_toolkit.styles import Style
+
+style = Style.from_dict({
+    # User input (default text).
+    '':          '#ff0066',
+
+    # Prompt.
+    'username': '#884444',
+    'at':       '#00aa00',
+    'colon':    '#0000aa',
+    'pound':    '#00aa00',
+    'host':     '#00ffff bg:#444400',
+    'path':     'ansicyan underline',
+})
+
+message = [
+    ('class:username', 'johndo'),
+    ('class:at',       '@'),
+]
+
+
+
 if __name__ == "__main__":
     bindings = KeyBindings()
 
@@ -23,11 +48,4 @@ if __name__ == "__main__":
 # wait for keypress
     session = PromptSession()
     while True:
-        session.prompt(key_bindings=bindings)
-
-
-
-
-
-
-# colorise output
+        session.prompt(message, style=style, key_bindings=bindings)
